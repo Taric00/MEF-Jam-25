fa07ad8efd496665a885be57be789eee4752d2f1
Mehmet Tarık Albayrak <albayrakmehmettarik@gmail.com>
2025-05-10 07:46:35 +0300

    ilkGunSonu

---
+++ MEF-Jam-25/Assets/Taric/Scripts_T/GameManagerT.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerT : MonoBehaviour
+{
+    public static GameManagerT instance;
+
+    [Header("Score")]
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int scorePerGoodNote = 125;
+    public int scorePerPerfectNote = 175;
+    [SerializeField] Text scoreText;
+
+
+    void Start()
+    {
+        instance = this;
+        scoreText.text = "score: 0";
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit on Time");
+        scoreText.text = "score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote;
+        NoteHit();
+    }
+
+    public void NoteMissed()
+    {   
+        Debug.Log("Miss");
+    }
+}
--- MEF-Jam-25/Assets/Taric/Scripts_T/NoteObject.cs
+++ MEF-Jam-25/Assets/Taric/Scripts_T/NoteObject.cs
@@ -4,20 +4,50 @@ public class NoteObject : MonoBehaviour
 {
     public bool canBePressed;
     public KeyCode keyToPress;
+    [SerializeField] private GameObject hitEffect;
+
+    [Header("Debug")]
+    private bool hasBeenHit = false;
+
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(keyToPress))
+        if (Input.GetKeyDown(keyToPress))
         {
-            if(canBePressed)
+            if (canBePressed && !hasBeenHit)
             {
+                hasBeenHit = true;
                 gameObject.SetActive(false);
+                float yPos = Mathf.Abs(transform.position.y);
+
+
+                //---Uzaklýk Hesabýna Göre Puan Daðýtma---//
+                if (yPos > 0.7f)
+                {
+                    Debug.Log("normal Hit!");
+                    GameManagerT.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+
+                else if (yPos > 0.3f)
+                {
+                    Debug.Log("good Hit!");
+                    GameManagerT.instance.GoodHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+
+                }
+
+                else
+                {
+                    Debug.Log("perfect Hit!");
+                    GameManagerT.instance.PerfectHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
             }
         }
     }
@@ -32,9 +62,10 @@ public class NoteObject : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.tag == "Activator")
+        if (other.tag == "Activator" && !hasBeenHit)
         {
             canBePressed = false;
+            GameManagerT.instance.NoteMissed();
         }
     }
 }
